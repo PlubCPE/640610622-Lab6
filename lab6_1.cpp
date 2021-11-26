7ef186a2bed62b3f2195e23063b59073ee61dfdb
PlubCPE <chakkapop_s@cmu.ac.th>
2021-11-26 16:19:09 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,28 @@
 using namespace std;
 
 int main(){
+    int odd = 0, even = 0, i;
+    
+
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin  >> i;
+    
+    while(i != 0)
+    {
+        if(i%2 == 0)
+        {
+            even = even + 1;
+        }
+        else
+        {
+            odd = odd +1;
+        }
+         cout << "Enter an integer: ";
+         cin  >> i;
+    }
+    
+   
+    cout << "#Even numbers = " << even << endl;
+    cout << "#Odd numbers = " << odd;
     return 0;
 }
